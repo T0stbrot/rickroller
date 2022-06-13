8ecca72f409e0a0d2269e7109ed7c8262ff8bf94
ye_ <51487454+ye01011001@users.noreply.github.com>
2022-06-13 16:11:29 +0200

    Name this file Something your friend would open!
    i added some code that does nothing except that if you right click > edit looks like its doing something other that opening a url

---
+++ game.bat
@@ -0,0 +1,77 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAJRczimOXixEroM1O/64u+7q04SWqw2e4C7
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdFy5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdFy5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZks0
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
+::ZQ05rAF9IAHYFVzEqQIDJhpfSWQ=
+::eg0/rx1wNQPfEVWB+kM9LVsJDAODMW7a
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQITIB5XR0SNKX/6C6xcz+n26oo=
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAJRczimOXixEroM1O/64u+7pUwQUaw6YIq7
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+::if (youtube = browserDefault) {
+::    open (youtube) as [
+::        loginID: 93457nbbggw23900sd
+::    ]
+::}
+::if(youtube == youtube) {
+::    youtube = yt
+::}
+::loop {
+::    if loop != noLoop {
+::        do :loopFinal
+::    }
+::}
+
+:::loopFinal
+::if {noLoop = True} [
+::    enableLoop
+::]else
+::return;
+
+::if loopFinal(finished) {
+::    loopFinal set (finished) {
+::        loop
+::    }
+::}else
+::return(1);
+
+::set return[1,2,3] {
+::    array(1,2,3)
+::    setArrayForLoop[array]
+::}return;
+
+:: set array[return] {
+::    return loopFinal
+::    set loop [
+::        loopFinal
+::    ]
+::}
+
+explorer https://youtu.be/2EkXa0GMH1w
+cls
+echo heeh heeh
+exit
